f97f9b7979a343a835bd386fcf18940d32d1d2dc
Faisal Manzer <faisal_manzer@yahoo.in>
2018-10-20 19:32:29 +0530

    Update card_deck.py

--- card_deck.py
+++ card_deck.py
@@ -18,10 +18,14 @@ for ti in range(0, T):
 
     NUMBERS = [int(i) for i in input().split(" ")]
 
-    first_increasing = check_increasing(NUMBERS)
-    second_increasing = check_increasing(NUMBERS[first_increasing + 1:])
+    if NUMBERS[0] > NUMBERS[-1]:
 
-    if first_increasing + second_increasing == len(NUMBERS) - 2:
-        print("YES")
+        first_increasing = check_increasing(NUMBERS)
+        second_increasing = check_increasing(NUMBERS[first_increasing + 1:])
+
+        if first_increasing + second_increasing == len(NUMBERS) - 2:
+            print("YES")
+        else:
+            print("NO")
     else:
         print("NO")
